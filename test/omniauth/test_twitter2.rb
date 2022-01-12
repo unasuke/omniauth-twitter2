5ce9aac3b274257cc69592cb68e8d2609ee1ca45
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-01-13 03:22:10 +0900

    Very simple test

--- test/omniauth/test_twitter2.rb
+++ test/omniauth/test_twitter2.rb
@@ -1,13 +1,56 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "omniauth"
 
 class TestOmniAuthTwitter2 < Minitest::Test
+  def setup
+    OmniAuth.config.test_mode = true
+    # OmniAuth.config.mock_auth[:twitter2] = OmniAuth::AuthHash.new(mock_auth_sample)
+  end
+
+  def teardown
+    OmniAuth.config.test_mode = false
+  end
+
+  def strategy(options = {})
+    OmniAuth::Strategies::Twitter2.new("CLIENT_ID", "CLIENT_SECRET", options)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::OmniAuth::Twitter2::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_has_strategy_name_twitter2
+    assert_equal "twitter2", strategy.options.name
+  end
+
+  def test_it_has_correct_site
+    assert_equal "https://api.twitter.com", strategy.options.client_options.site
+  end
+
+  def test_it_has_correct_token_url
+    assert_equal "2/oauth2/token", strategy.options.client_options.token_url
+  end
+
+  def test_it_has_correct_authorize_url
+    assert_equal "https://twitter.com/i/oauth2/authorize", strategy.options.client_options.authorize_url
+  end
+
+  def test_it_has_correct_info_hash
+    subject = strategy
+    subject.stub(:raw_info, raw_info_sample) do
+      assert_equal "うなすけ", subject.info[:name]
+      assert_nil subject.info[:email]
+      assert_equal "yu_suke1994", subject.info[:nickname]
+      assert_equal "帰って寝たい", subject.info[:description]
+    end
+  end
+
+  def test_it_has_uid
+    subject = strategy
+    subject.stub(:raw_info, raw_info_sample) do
+      assert_equal "108252390", subject.uid
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,60 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "omniauth/twitter2"
 
 require "minitest/autorun"
+
+def mock_auth_sample # rubocop:disable Metrics/MethodLength
+  # 2022-01-13 02:40:00 +0900
+  { "provider" => "twitter2",
+    "uid" => "108252390",
+    "info" => {
+      "name" => "うなすけ",
+      "email" => nil,
+      "nickname" => "yu_suke1994",
+      "description" => "帰って寝たい",
+      "image" => "https://pbs.twimg.com/profile_images/580019517608218624/KzEZSzUy_normal.jpg",
+      "urls" => {
+        "Website" => "https://t.co/NCFLB8wDkx",
+        "Twitter" => "https://twitter.com/yu_suke1994"
+      }
+    },
+    "credentials" => {
+      "token" => "TOKENTOKENTOKENTOKENTOKENTOKEN",
+      "expires_at" => 1_642_016_242,
+      "expires" => true
+    },
+    "extra" => {
+      "raw_info" => raw_info_sample
+    } }
+end
+
+def raw_info_sample # rubocop:disable Metrics/MethodLength
+  {
+    "data" => {
+      "profile_image_url" => "https://pbs.twimg.com/profile_images/580019517608218624/KzEZSzUy_normal.jpg",
+      "url" => "https://t.co/NCFLB8wDkx",
+      "public_metrics" => {
+        "followers_count" => 2188,
+        "following_count" => 1478,
+        "tweet_count" => 162_937,
+        "listed_count" => 110
+      },
+      "verified" => false,
+      "name" => "うなすけ",
+      "entities" => {
+        "url" => {
+          "urls" => [{
+            "start" => 0,
+            "end" => 23,
+            "url" => "https://t.co/NCFLB8wDkx",
+            "expanded_url" => "https://unasuke.com", "display_url" => "unasuke.com"
+          }]
+        }
+      },
+      "description" => "帰って寝たい",
+      "created_at" => "2010-01-25T10:10:22.000Z",
+      "username" => "yu_suke1994",
+      "protected" => false,
+      "id" => "108252390"
+    }
+  }
+end
